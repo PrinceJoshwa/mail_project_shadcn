11f694db8e6f467d5fc90d59621b1114ddefb62d
PrinceJoshwa <sprincejoshwa85@gmail.com>
2024-08-27 20:54:16 +0530

    Add Button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
 'use client'
 
+import { BellIcon } from "lucide-react"
 import { CommandDemo } from "./Command"
+import { Button } from "./ui/button"
 
 export default function Header() {
-    return <div className="flex gap-4 p-4 border-b">
+    return <div className="grid grid-cols-2 gap-4 p-4 border-b">
         <CommandDemo />
+        <div>
+            <Button variant="outline" size="icon">
+                <BellIcon className="h-4 w-4" />
+            </Button>
+        </div>
     </div>
 }
